4428b6e74806846008f64b6889e4849daab92b67
dreiv <dreivoicu@gmail.com>
2017-11-08 19:55:22 +0200

    called `https://jsonplaceholder.typicode.com` through HttpClient.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
-    template: `<h1>hello world!</h1>`,
+    templateUrl: './app.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+
+    constructor(private httpClient: HttpClient) {
+    }
+
+    valid() {
+        this.get();
+    }
+
+    invalid() {
+        this.get('12');
+    }
+
+    private get(endpoint = '') {
+        this.httpClient.get(`https://jsonplaceholder.typicode.com/users${endpoint}`)
+            .subscribe(success => {
+                    console.log('Successfully Completed');
+                    console.log(success);
+                }
+            );
+    }
 }
